0fe510e350ad70298d7d6f66c3c73a1f705c06a4
Veniamin_Bakhar <Veniamin_Bakhar@epam.com>
2021-10-11 23:08:21 +0300

    implemented equals and hashcode methods for CurrencyPairRate

--- src/main/java/model/CurrencyPairRate.java
+++ src/main/java/model/CurrencyPairRate.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class CurrencyPairRate {
     private String ccyPair;
     private double rate;
@@ -19,5 +21,26 @@ public class CurrencyPairRate {
 
     public void setRate(double rate) {
         this.rate = rate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CurrencyPairRate pairRate = (CurrencyPairRate) o;
+
+        if (Double.compare(pairRate.rate, rate) != 0) return false;
+        return Objects.equals(ccyPair, pairRate.ccyPair);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = ccyPair != null ? ccyPair.hashCode() : 0;
+        temp = Double.doubleToLongBits(rate);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
